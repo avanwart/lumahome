b2bbc149da64ee76fd1f419a269709c5a395d880
andywarthol <andywarthol@gmail.com>
2016-10-06 11:51:06 -0700

    Moving GA, Optimizely, Hotjar to head which is a best practice

--- base.php
+++ base.php
@@ -29,56 +29,6 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/footer');
       wp_footer();
     ?>
-<!-- start analytics -->
-
-<script type="text/javascript">
-
-      var $buoop = {vs:{i:9,f:25,o:12.1,s:7},c:2};
-      function $buo_f(){
-       var e = document.createElement("script");
-       e.src = "//browser-update.org/update.min.js";
-       document.body.appendChild(e);
-      };
-      try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-      catch(e){window.attachEvent("onload", $buo_f)}
-    
-/*!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";analytics.load("dYNCKt7rqB8Y7mUDzPlRb1Kg5YvmBRkB");analytics.page()}}();
-    
-    adroll_adv_id = "UKNLAVEPQZD35C6QE2PLJT";
-    adroll_pix_id = "A45GBDVKGVAZ7EFHWMDC3F";
-    (function () {
-        var _onload = function(){
-            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
-            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
-            var scr = document.createElement("script");
-            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-            scr.setAttribute('async', 'true');
-            scr.type = "text/javascript";
-            scr.src = host + "/j/roundtrip.js";
-            ((document.getElementsByTagName('head') || [null])[0] ||
-                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-        };
-        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
-        else {window.attachEvent('onload', _onload)}
-    }()); */
-    
-<!-- Begin Google Analytics -->
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-69680118-1', 'auto');
- window.optimizely = window.optimizely || [];
- window.optimizely.push("activateUniversalAnalytics");
-  ga('send', 'pageview');
- 
-   <!-- End Google Analytics -->
-  </script>
-  
-   <!-- Optimizely Snippet -->
-   <script src="https://cdn.optimizely.com/js/5614381003.js"></script>
 
 <script>
 
--- templates/head.php
+++ templates/head.php
@@ -6,6 +6,50 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300" rel="stylesheet" type="text/css"/>
   <?php wp_head(); ?>
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/styles/css/child.css">
+
+  <!-- start analytics -->
+	<script type="text/javascript">
+
+	      var $buoop = {vs:{i:9,f:25,o:12.1,s:7},c:2};
+	      function $buo_f(){
+	       var e = document.createElement("script");
+	       e.src = "//browser-update.org/update.min.js";
+	       document.body.appendChild(e);
+	      };
+	      try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+	      catch(e){window.attachEvent("onload", $buo_f)}
+	</script>
+	    
+	<!-- Begin Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-69680118-1', 'auto');
+	 window.optimizely = window.optimizely || [];
+	 window.optimizely.push("activateUniversalAnalytics");
+	  ga('send', 'pageview');
+	 
+	</script>
+	<!-- End Google Analytics -->
+
+	<!-- Optimizely Snippet -->
+	<script src="https://cdn.optimizely.com/js/5614381003.js"></script>
+
+	<!-- Hotjar Tracking Code for https://getluma.com -->
+	<script>
+	  (function(h,o,t,j,a,r){
+	      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	      h._hjSettings={hjid:169072,hjsv:5};
+	      a=o.getElementsByTagName('head')[0];
+	      r=o.createElement('script');r.async=1;
+	      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+	      a.appendChild(r);
+	  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
+	
   <!-- Facebook Pixel Code -->
 	<script>
 	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
